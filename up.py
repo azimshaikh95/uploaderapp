f58c1efd42249061199d2267546e7a9a443a3291
Azim <azim.music9@gmail.com>
2022-05-14 12:57:14 +0530

    Updated on 2022-05-14 12:57:14

--- up.py
+++ up.py
@@ -1,19 +1,30 @@
 import streamlit as st
-import pandas as pd
+from github import Github
 
-df = pd.read_csv("data/abc.csv")
+g = Github( st.secrets["username"] , st.secrets["pswd"] )
+g = Github( st.secrets["key"] )
 
-@st.cache
-def convert_df(df):
-   return df.to_csv().encode('utf-8')
+repo = g.get_user().get_repo(uploaderapp)
+all_files = []
+contents = repo.get_contents("")
+while contents:
+    file_content = contents.pop(0)
+    if file_content.type == "dir":
+        contents.extend(repo.get_contents(file_content.path))
+    else:
+        file = file_content
+        all_files.append(str(file).replace('ContentFile(path="','').replace('")',''))
 
+with open('/tmp/file.txt', 'r') as file:
+    content = file.read()
 
-csv = convert_df(df)
-
-st.download_button(
-   "Press to Download",
-   csv,
-   "file.csv",
-   "text/csv",
-   key='download-csv'
-)
+# Upload to github
+git_prefix = 'folder1/'
+git_file = git_prefix + 'file.txt'
+if git_file in all_files:
+    contents = repo.get_contents(git_file)
+    repo.update_file(contents.path, "committing files", content, contents.sha, branch="master")
+    print(git_file + ' UPDATED')
+else:
+    repo.create_file(git_file, "committing files", content, branch="master")
+    print(git_file + ' CREATED')
